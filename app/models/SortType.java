90aab09c231ad48dd419a563f9668fd5cf5d6b2d
Andrew <andrewc92@aol.com>
2013-06-17 16:10:10 -0400

    adding sortType class and minor modifications ( a date) to the run class

---
+++ app/models/SortType.java
@@ -0,0 +1,45 @@
+package models;
+
+/**
+ * Class of SortTypes. Made to populate list in "listruns".
+ * Examples of Sort Types include Date, SVN, etc.
+ * @author acarey
+ *
+ */
+public class SortType {
+	/**
+	Part of the Sort Type that the filter uses. 
+	 */
+	private String value;
+	
+	/**
+	Header of the Sort Type.  
+	 */
+	private String header;
+	
+	/**
+	 * 
+	 * @param v value of Sort Type.
+	 * @param h header of Sort Type.
+	 */
+	
+	public SortType(String v, String h) {
+		value = v;
+		header = h;
+					}
+	/**
+	 * Returns header.
+	 * @return header.
+	 */
+	public String getHeader() {
+		return header;
+	}
+	/**
+	 * returns value.
+	 * @return value.
+	 */
+	public String getValue() {
+		return value;
+	}
+	
+}
